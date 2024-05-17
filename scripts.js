39c51c95edb6ba59b4d5f4e93c6de9d6a7b9e11e
mystique4u <yurii.masalitin@powercoders.org>
2024-05-17 09:17:58 +0200

    added authors image

---
+++ scripts.js
@@ -0,0 +1,62 @@
+let authors = [
+    {
+        firstName: 'Andreas',
+        lastName: 'Neeser',
+        imageURL: 'img/tile_andreas_neeser.jpg'
+    },
+    {
+        firstName: 'Anna',
+        lastName: 'Ruchat',
+        imageURL: 'img/tile_anna_ruchat.jpg'
+    },
+    {
+        firstName: 'Arno',
+        lastName: 'Camenisch',
+        imageURL: 'img/tile_arno_camenisch.jpg'
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Schibli',
+        imageURL: 'img/tile_barbara_schibli.jpg'
+    },
+    {
+        firstName: 'Demian',
+        lastName: 'Leinhard',
+        imageURL: 'img/tile_demian_leinhard.jpg'
+    },
+    {
+        firstName: 'Flurina',
+        lastName: 'Bader',
+        imageURL: 'img/tile_flurina_bader.jpg'
+    },
+    {
+        firstName: 'Franco',
+        lastName: 'Supino',
+        imageURL: 'img/tile_franco_supino.jpg'
+    },
+    {
+        firstName: 'Lukas',
+        lastName: 'Hartmann',
+        imageURL: 'img/tile_lukas_hartmann.jpg'
+    },
+    {
+        firstName: 'Marius',
+        lastName: 'Popescu',
+        imageURL: 'img/tile_marius_popescu.jpg'
+    },
+    {
+        firstName: 'Reto',
+        lastName: 'Haenny',
+        imageURL: 'img/tile_reto_haenny.jpg'
+    },
+    {
+        firstName: 'Sandra',
+        lastName: 'Kuenzi',
+        imageURL: 'img/tile_sandra_kuenzi.jpg'
+    },
+    {
+        firstName: 'Simon',
+        lastName: 'Libsig',
+        imageURL: 'img/tile_simon_libsig.jpg'
+    }
+];
